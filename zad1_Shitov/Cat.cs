767c9f829f09c27b99a587c25b50a13f2b6c6682
AiromanR <andrey_rogov_06@mail.ru>
2026-06-06 13:15:10 +0500

    zad1_Shitov

---
+++ zad1_Shitov/Cat.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1_Shitov
+{
+    internal class Cat
+    {
+        private string name;
+        private double weight;
+        public Cat(string CatName, double CatWeight) { Name = CatName; Weight = CatWeight; }
+        public void Meow() { Console.WriteLine($"{name}: МЯЯЯЯУ!!!!"); }
+        public void CatInfo()
+        {
+            Console.WriteLine($"{name}, вес: {weight} кг");
+        }
+        public void SetCatName(string CatName)
+        {
+            bool OnlyLetters = true;
+            foreach (var ch in CatName)
+            {
+                if (!char.IsLetter(ch)) 
+                {
+                    OnlyLetters = false; 
+                }
+            } 
+            if (OnlyLetters) name = CatName;
+            else
+                Console.WriteLine($"{CatName} - неправильное имя!!!"); 
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                bool onlyLetters = true;
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Имя не может быть пустым!!!");
+                    name = "ERROR";
+                    return;
+                }
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        onlyLetters = false;
+                        break;
+                    }
+                }
+                if (onlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                    name = "ERROR";
+                }
+            }
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0.1 && value < 15.0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - Неправильный вес!!!");
+                    weight = -1;
+                }
+            }
+        }
+    }
+}
---
+++ zad1_Shitov/Program.cs
@@ -0,0 +1,66 @@
+﻿using zad1_Shitov;
+
+class Program 
+{   
+    static void Main(string[] args)
+    {
+        /*Cat murzik = new Cat("Мурзик", 6.01);
+        Cat barsik = new Cat("Барсег", 4.21);
+        murzik.Meow();
+        barsik.Meow(); 
+        barsik.Name = "Барсик";
+        barsik.Meow();
+        barsik.Name = "1234";
+        barsik.Meow();*/
+        int count = 0;
+        string name;
+        double weight;
+        do
+        {
+            Console.WriteLine("Сколько котов хотите ввети?");
+            count = Convert.ToInt32(Console.ReadLine());
+        } while (count < 1);
+        Cat[] cats = new Cat[count];
+        Console.WriteLine($"===Заполнение {count} котов===");
+        for (int i = 0; i < count; i++)
+        {
+            Cat tempCat = new Cat("temp", 1.0);
+            do
+            {
+                Console.Write($"Введите имя {i + 1} кота: ");
+                string inputName = Console.ReadLine();
+                tempCat.Name = inputName;
+
+            } while (tempCat.Name == "ERROR");
+            do
+            {
+                Console.Write($"Введите вес {i + 1} кота (кг): ");
+                string input = Console.ReadLine();
+
+                double inputWeight;
+                if (!double.TryParse(input, out inputWeight))
+                {
+                    inputWeight = -1;
+                }
+
+                tempCat.Weight = inputWeight;
+
+            } while (tempCat.Weight == -1);
+
+            cats[i] = new Cat(tempCat.Name, tempCat.Weight);
+        }
+
+        Console.WriteLine("\n=== Информация о котах ===");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Кот {i + 1}: ");
+            cats[i].CatInfo(); 
+        }
+
+        Console.WriteLine("\n=== Все коты мяукают ===");
+        for (int i = 0; i < count; i++)
+        {
+            cats[i].Meow();
+        }
+    }   
+}
